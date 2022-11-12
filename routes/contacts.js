316f8c5c3f88d302302c3722311b5e048edd0ca1
alexesca <alexescamore@gmail.com>
2022-11-11 22:12:49 -0700

    Removed docs

--- routes/contacts.js
+++ routes/contacts.js
@@ -8,21 +8,7 @@ const router = express.Router();
 
 // Home page route.
 router
-    /**
-     * @swagger
-     * /:
-     *   get:
-     *     summary: Retrieve a list of JSONPlaceholder users
-     *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
-     */
     .get("/", controller.index)
-    /**
-     * @swagger
-     * /_id:
-     *   get:
-     *     summary: Retrieve a list of JSONPlaceholder users
-     *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
-     */
     .get("/:_id", controller.id)
     .post("/", contactMiddlewares.createContact, controller.create)
     .put("/:_id", contactMiddlewares.createContact, controller.update)
