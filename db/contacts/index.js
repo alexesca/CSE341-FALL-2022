546ef9a181283dd187ce6995e14f35e0cc8ff131
alexesca <alexescamore@gmail.com>
2022-11-12 15:51:24 -0700

    Changed schema

--- db/contacts/index.js
+++ db/contacts/index.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ContactSchema = new Schema({
-    name: String,
+    birthday: String,
     email: String,
-    phoneNumber: String,
-    dateOfBirth: String,
     favoriteColor: String
+    firstName: String,
+    lastName: String,
 });
 
 const ContactModel = mongoose.model('Contact', ContactSchema);
