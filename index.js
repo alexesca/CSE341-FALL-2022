5393f864b41ce0eaa54c973f943c07e425e33092
alexesca <alexescamore@gmail.com>
2022-11-12 15:37:36 -0700

    Added cors() back

--- index.js
+++ index.js
@@ -11,15 +11,7 @@ var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-    );
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-})
+app.use(cors())
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", routes);
