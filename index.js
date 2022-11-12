f9d279173a3acfb2b14b9d07fe071b77a0b79728
alexesca <alexescamore@gmail.com>
2022-11-12 15:32:08 -0700

    Added corse middleware

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json())
-app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+})
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", routes);
