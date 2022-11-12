d7c5b3fe7194c5b36303d878a6e0fba7469f311e
alexesca <alexescamore@gmail.com>
2022-11-11 23:35:40 -0700

    Added corse options

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 var cors = require('cors')
 
+var corsOptions = {
+    origin: 'https://css341-fall-2022.onrender.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 const app = express()
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/", routes);
